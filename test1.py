9b8f9b65129f192af52d7748a4d6672971c6a3ea
RomanDiachenko <52920901+RomanDiachenko@users.noreply.github.com>
2019-08-03 18:58:41 +0300

    works code

--- test1.py
+++ test1.py
@@ -2,7 +2,6 @@ import time
 import unittest
 
 from selenium import webdriver
-
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 
@@ -13,40 +12,62 @@ class OpenBrowser(unittest.TestCase):
         self.driver.maximize_window()
         self.driver.implicitly_wait(10)
 
-    def test_firsttap(self):
+    def open_page(self):
         driver = self.driver
         driver.get("https://athletictrainers.inloop.com")
-        new_page = driver.find_element_by_tag_name('html')
-        self.scroll_to_newsletter(driver)
-        self.click_in_first_newsletter(driver)
-        self.jump_to_paje()
+        driver.find_element_by_tag_name('html')
 
-    def jump_to_paje(self):
-        s = self.driver.window_handles
-        print(s)
-        b = self.driver.current_window_handle
-        print(b)
-        time.sleep(2)
-        self.driver.switch_to.window(b)
-        time.sleep(2)
+    def my_tests(self):
+        self.open_page()
+        self.scroll_to_newsletter()
+        self.click_in_first_newsletter()
+        self.test_jump_to_page("/en/api/feedlandingapi/getnewslettertemplatebody?id=6708")
+        # self.open_some_tab()
 
-    def click_in_first_newsletter(self, driver):
+    def test_jump_to_page(self, href_path):
+        second_tab = self.driver.window_handles[2]
+        print(second_tab)
+        first_tab = self.driver.current_window_handle
+        print(first_tab)
+        time.sleep(2)
+        self.driver.switch_to.window(second_tab)
+        time.sleep(2)
+        page_url = self.driver.current_url
+        print(page_url)
+        expected_url = "https://athletictrainers.inloop.com%s" % href_path
+        assert expected_url == page_url
+
+    def click_in_first_newsletter(self):
+        driver = self.driver
         driver.find_element_by_css_selector(
             "body.ng-scope.ng-isolate-scope:nth-child(2) div.mm-page.mm-slideout:nth-child(4) div.flex-wrapper.ng-scope:nth-child(1) template-page.ng-isolate-scope div.container.main-wrapper div.margin-top-1.main-content.mag-content.clearfix div.row.ng-scope:nth-child(2) div.col-md-4.ng-scope div.ng-scope div.ng-scope newsletters-archive.ng-isolate-scope:nth-child(13) div.widget.reviewwidget.newsletters div:nth-child(2) article.widget-post.clearfix.ng-scope:nth-child(1) header:nth-child(2) h3.ng-scope > a.ng-binding").click()
-        time.sleep(2)
-        a = self.driver.current_url
-        print(a)
-        current_url = "https://athletictrainers.inloop.com/en/api/feedlandingapi/getnewslettertemplatebody?id=6708"
-        assert current_url == "https://athletictrainers.inloop.com/en/api/feedlandingapi/getnewslettertemplatebody?id=6708"
+        time.sleep(10)
 
-    def scroll_to_newsletter(self, driver):
+    def scroll_to_newsletter(self):
+        driver = self.driver
         element = driver.find_element_by_css_selector(
-            '''body.ng-scope.ng-isolate-scope:nth-child(2) div.mm-page.mm-slideout:nth-child(4) div.flex-wrapper.ng-scope:nth-child(1) template-page.ng-isolate-scope div.container.main-wrapper div.margin-top-1.main-content.mag-content.clearfix div.row.ng-scope:nth-child(2) div.col-md-4.ng-scope div.ng-scope div.ng-scope newsletters-archive.ng-isolate-scope:nth-child(13) div.widget.reviewwidget.newsletters div:nth-child(2) article.widget-post.clearfix.ng-scope:nth-child(1) header:nth-child(2) h3.ng-scope > a.ng-binding''')
+            "body.ng-scope.ng-isolate-scope:nth-child(2) div.mm-page.mm-slideout:nth-child(4) div.flex-wrapper.ng-scope:nth-child(1) template-page.ng-isolate-scope div.container.main-wrapper div.margin-top-1.main-content.mag-content.clearfix div.row.ng-scope:nth-child(2) div.col-md-4.ng-scope div.ng-scope div.ng-scope newsletters-archive.ng-isolate-scope:nth-child(13) div.widget.reviewwidget.newsletters div:nth-child(2) article.widget-post.clearfix.ng-scope:nth-child(1) header:nth-child(2) h3.ng-scope > a.ng-binding")
         ActionChains(driver).move_to_element(element).click().send_keys(Keys.PAGE_DOWN).perform()
         time.sleep(2)
 
-    def tear_down(self):
-        self.driver.quit()
+    def test_open_some_tab(self):
+        driver = self.driver
+        second_tab = self.driver.window_handles
+        print(second_tab)
+        first_tab = self.driver.current_window_handle
+        print(first_tab)
+        driver.find_element_by_css_selector("body.ng-scope.ng-isolate-scope:nth-child(3) div.mm-page.mm-slideout:nth-child(4) div.flex-wrapper.ng-scope:nth-child(1) template-page.ng-isolate-scope div.container.main-wrapper div.margin-top-1.main-content.mag-content.clearfix div.row.ng-scope:nth-child(2) div.col-md-4.ng-scope div.ng-scope div.ng-scope newsletters-archive.ng-isolate-scope:nth-child(13) div.widget.reviewwidget.newsletters div:nth-child(2) article.widget-post.clearfix.ng-scope:nth-child(2) header:nth-child(2) h3.ng-scope > a.ng-binding").click()
+        time.sleep(2)
+        self.driver.switch_to.window(first_tab)
+        driver.find_element_by_css_selector("body.ng-scope.ng-isolate-scope:nth-child(3) div.mm-page.mm-slideout:nth-child(4) div.flex-wrapper.ng-scope:nth-child(1) template-page.ng-isolate-scope div.container.main-wrapper div.margin-top-1.main-content.mag-content.clearfix div.row.ng-scope:nth-child(2) div.col-md-4.ng-scope div.ng-scope div.ng-scope newsletters-archive.ng-isolate-scope:nth-child(13) div.widget.reviewwidget.newsletters div:nth-child(2) article.widget-post.clearfix.ng-scope:nth-child(3) header:nth-child(2) h3.ng-scope > a.ng-binding").click()
+        self.driver.switch_to.window(first_tab)
+        driver.find_element_by_css_selector("body.ng-scope.ng-isolate-scope:nth-child(3) div.mm-page.mm-slideout:nth-child(4) div.flex-wrapper.ng-scope:nth-child(1) template-page.ng-isolate-scope div.container.main-wrapper div.margin-top-1.main-content.mag-content.clearfix div.row.ng-scope:nth-child(2) div.col-md-4.ng-scope div.ng-scope div.ng-scope newsletters-archive.ng-isolate-scope:nth-child(13) div.widget.reviewwidget.newsletters div:nth-child(2) article.widget-post.clearfix.ng-scope:nth-child(4) header:nth-child(2) h3.ng-scope > a.ng-binding").click()
+        self.driver.switch_to.window(first_tab)
+        driver.find_element_by_css_selector("body.ng-scope.ng-isolate-scope:nth-child(3) div.mm-page.mm-slideout:nth-child(4) div.flex-wrapper.ng-scope:nth-child(1) template-page.ng-isolate-scope div.container.main-wrapper div.margin-top-1.main-content.mag-content.clearfix div.row.ng-scope:nth-child(2) div.col-md-4.ng-scope div.ng-scope div.ng-scope newsletters-archive.ng-isolate-scope:nth-child(13) div.widget.reviewwidget.newsletters div:nth-child(2) article.widget-post.clearfix.ng-scope:nth-child(5) header:nth-child(2) h3.ng-scope > a.ng-binding").click()
+        self.driver.switch_to.window(first_tab)
+
+def tear_down(self):
+    self.driver.quit()
 
 
 if __name__ == "__main__":
